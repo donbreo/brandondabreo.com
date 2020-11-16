f57722d172fa272f0f91d4673e544c85e1e2bf8b
donbreo <donbreo@gmail.com>
2020-11-17 01:34:16 +0530

    port number change

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = "Brandon D'Abreo"
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = "Brandon D'Abreo"
 
 export default function Layout({ children, home }) {
   return (
