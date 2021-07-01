7db7a29a1bb1a305a9e8a976abda1e0648560e67
donbreo <donbreo@gmail.com>
2021-07-02 04:43:12 +0530

    pm2 config watcher added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,10 @@ module.exports = {
   apps : [{
     name   : "app1",
     script : "npm",
-    args : "start"
+    args : "start",
+    watch: ["components","lib", "pages","posts","public","styles"],
+    // Delay between restart
+    watch_delay: 1000,
+    ignore_watch : ["node_modules", ".next"],
   }]
 }
